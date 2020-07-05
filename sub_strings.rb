c35b04b19173f55e2becd54c3a5a14f02dc5d280
yokn <discolouredattic@gmail.com>
2020-07-05 14:05:47 +0300

    working for single word

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,20 +3,17 @@
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 # Get the word(s) and the array of substrings from the user
-def substrings(words, _dict)
+def substrings(words, dict)
   i = 0
   word_array = [words]
-  puts word_array
+  result_array = []
   # For every word, iterate through the substring array to see if there is a match
   # If there is a match add one to a counter for that word
-  puts word_array.reduce do |acc, word|
-    next unless word_array[i].to_s == word.to_s
-
-    acc[word] += 1
-    puts `'ello!`
+  while i < word_array.length
+    result_array[i] = word_array[i] if dict.include?(word_array[i])
+    i += 1
   end
-  i += 1
-  puts 'hihihi'
+  puts result_array.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1; }
 end
 # Return a hash
 substrings('below', dictionary)
