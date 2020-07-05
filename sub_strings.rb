39c6588bfeda3e73ccfbb4a75d5e35b3bc2ce549
yokn <discolouredattic@gmail.com>
2020-07-05 14:17:09 +0300

    working with multiple words

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,9 +3,9 @@
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 # Get the word(s) and the array of substrings from the user
-def substrings(words, dict)
+def substrings(*words, dict)
   i = 0
-  word_array = [words]
+  word_array = [*words]
   result_array = []
   # For every word, iterate through the substring array to see if there is a match
   # If there is a match add one to a counter for that word
@@ -16,4 +16,4 @@ def substrings(words, dict)
   puts result_array.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1; }
 end
 # Return a hash
-substrings('below', dictionary)
+substrings('below', 'down', 'this string does not exist', dictionary)
