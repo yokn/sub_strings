948fce51cf55047356f065a578b5bc2b7e182b8e
yokn <discolouredattic@gmail.com>
2020-07-05 14:34:37 +0300

    need to add normalization

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-
 # Get the word(s) and the array of substrings from the user
-def substrings(*words, dict)
+def substrings(words, dict)
   i = 0
-  word_array = [*words]
+  word_array = words.split
   result_array = []
-  # For every word, iterate through the substring array to see if there is a match
-  # If there is a match add one to a counter for that word
+  # For every word; iterate through the dictionary to see if there is a match
   while i < word_array.length
     result_array[i] = word_array[i] if dict.include?(word_array[i])
     i += 1
   end
+  # If there is a match; add one to a counter for that word
+  # Return a hash containing all the results
   puts result_array.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1; }
 end
-# Return a hash
-substrings('below', 'down', 'this string does not exist', dictionary)
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
